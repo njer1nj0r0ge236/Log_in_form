06ce7bcc78a4825a0b5ea376292506845979a85d
Njeri Njoroge <129849647+njer1nj0r0ge236@users.noreply.github.com>
2023-12-05 18:47:19 +0300

    Add files via upload

--- LOG IN FORM/script.js
+++ LOG IN FORM/script.js
@@ -1,1 +1,24 @@
-console.log("Hello Diana");
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('loginForm');
+  const emailInput = document.querySelector('input[type="email"]');
+  const passwordInput = document.querySelector('input[type="password"]');
+
+  form.addEventListener('submit', function (event) {
+      event.preventDefault(); 
+
+      // Get input values
+      const email = emailInput.value;
+      const password = passwordInput.value;
+
+      // For validation
+      if (email.trim() === '' || password.trim() === '') {
+          alert('Please enter both email and password.');
+      } else {
+          alert('Login successful!');
+      }
+  });
+});  
+  
+
